fc79deaaaae39fe5bc69d76598473eb02c6a9efa
KAYSILVHA <kaysilvha@gmail.com>
2023-10-26 21:02:50 -0300

    slq injection ataque

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
-const pool = require('./conexao')
+const pool = require('./conexao');
+
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/:id', async (req, res) => {
+  const {id} = req.params;
   try {
-    const resultado = await pool.query('select * from empresas');
-    return res.json(resultado)
+    const resultado = await pool.query(`select * from empresas where id = ${id}`);
+    return res.json(resultado.rows);
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
 });
 
 app.listen(3000, () => {
-  console.log("Sua Api está rodando na porta 3000")
+  console.log("Sua Api está rodando na porta 3000");
 });
+
+//passar na url http://localhost:3000/1 or 1=1 ao invez de trazer o 1 ele traz todos, isso é um  ataque de sql injection 
